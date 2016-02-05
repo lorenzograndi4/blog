46c41a14f7c0aaacc5f5a4949845bc8ea70e3bca
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2016-02-05 19:18:12 +0100

    css changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20160205101917) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
