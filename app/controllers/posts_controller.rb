d0be432c33b9ea1736b9743d9aed8d62a796cabf
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2016-02-05 12:28:26 +0100

    Create and list posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,31 @@
 
 class PostsController < ApplicationController
 
+  # A list of all posts
   def index
+  	@posts = Post.all
   end
 
+  # An empty Post form
+  def new
+  	@post = Post.new
+  end
 
+  # Create a Post from the form params
+  def create
+  
+  	if @post = Post.create(post_params)
+  	  redirect_to root_path # or action: :index
+  	else 
+      render :new
+    end
+  
+  end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 
 end
