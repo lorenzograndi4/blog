314fbefd76ca4bfe534bb3e1dcf48957d1dabbd2
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2016-02-05 15:06:15 +0100

    Added edit post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,24 @@ class PostsController < ApplicationController
   
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+
+  	if @post.update(post_params)
+  		redirect_to @post
+  	else
+  		render :edit
+  	end
+  end
+
   private
 
   def post_params
